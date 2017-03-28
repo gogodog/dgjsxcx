c4cdfd1bb7c4bdaa2da87a5ed8da4dc0c1bc6e17
jiadong.wen <jiadong.wen@corp.elong.com>
2017-03-28 17:58:14 +0800

    提交search页请求list

--- pages/search/search.js
+++ pages/search/search.js
@@ -24,24 +24,41 @@ Page({
   toList:function(_data){
     this.requests(_data,
     function(res){
-      console.log("请求数据成功：" + res.data)
-
-      res.results = [{name:'1',value:'lishi',key:'中国'},{name:'2',value:'lishi1',key:'美国'},{name:'3',value:'lishi2',key:'荷兰'}];
+      console.log("请求数据失败：" + msg.data);
+        res.results = [{name:'1',value:'lishi',key:'中国'},{name:'2',value:'lishi1',key:'美国'},{name:'3',value:'lishi2',key:'荷兰'}];
 
       var params = res.results;
       //去列表页面
       wx.navigateTo({
         url:"../list/list?params="+JSON.stringify(params),
-        success:function(m){
-          console.log('ok:' + JSON.stringify(m));
+        success:function(res){
+          console.log('ok:' + JSON.stringify(res));
         },
-        fail:function(m){
-          console.log('fail:' + JSON.stringify(m));
+        fail:function(res){
+          console.log('fail:' + JSON.stringify(res));
         },
-        complete:function(m){
-          console.log('complete:' + JSON.stringify(m));
+        complete:function(res){
+          console.log('complete:' + JSON.stringify(res));
         }
       })
-      },function(msg){console.log("请求数据失败：" + msg.data)});
+      },function(res){
+        console.log("请求数据失败：" + res.data);
+        res.results = [{name:'1',value:'lishi',key:'中国'},{name:'2',value:'lishi1',key:'美国'},{name:'3',value:'lishi2',key:'荷兰'}];
+
+      var params = res.results;
+      //去列表页面
+      wx.navigateTo({
+        url:"../list/list?params="+JSON.stringify(params),
+        success:function(res){
+          console.log('ok:' + JSON.stringify(res));
+        },
+        fail:function(res){
+          console.log('fail:' + JSON.stringify(res));
+        },
+        complete:function(res){
+          console.log('complete:' + JSON.stringify(res));
+        }
+      })
+      });
   }
 })
